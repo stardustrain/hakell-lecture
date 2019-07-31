ced3ce054036c7055580e4293f3b8b4230334519
stardustrain <kthanterran@gmail.com>
2019-07-31 22:30:35 +0900

    Week 1 homework.

---
+++ app/Main.hs
@@ -0,0 +1,6 @@
+module Main where
+
+import Lib
+
+main :: IO ()
+main = print (doubleEveryOther [2, 3, 4, 5, 6])
---
+++ src/Lib.hs
@@ -0,0 +1,29 @@
+module Lib
+    ( someFunc,
+      toDigits,
+      doubleEveryOther,
+      sumDigits
+    ) where
+
+someFunc :: IO ()
+someFunc = putStrLn "someFunc"
+
+toDigits :: Int -> [Int]
+toDigits 0 = []
+toDigits n = if n < 0
+                then []
+                else map (\x -> read[x] :: Int) (show n)
+
+toDigitsRev n = reverse (toDigits n)
+
+join :: [Int] -> String
+join xs = foldr (\a b -> (show a) ++ b) "" xs
+
+doubleEveryOther :: [Int] -> [Int]
+doubleEveryOther [] = []
+doubleEveryOther [x] = [x]
+doubleEveryOther (x:n:ns) = x:(n * 2) : (doubleEveryOther ns)
+
+sumDigits :: [Int] -> Int
+sumDigits xs = sum (toDigits (read (join xs) :: Int))
+
